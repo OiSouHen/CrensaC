6fe24ee52d1d01a53dc71c1243340d90561855b8
Lucas Hen <oisouhen@gmail.com>
2022-05-25 19:49:36 -0300

    Cfx maps

---
+++ server/resources/[cfx]/cfx-summerz/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version "cerulean"
+game "gta5"
+
+this_is_a_map "yes"
+
+files {
+	"stream/*",
+	"stream/*/**",
+	"stream/*/*/**",
+	"stream/*/*/*/**",
+	"interiorproxies.meta"
+}
+
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+
+data_file "DLC_ITYP_REQUEST" "stream/creativeForestNorth/forest_n_slod.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/creativeForestSouth/forest_s_slod.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "stream/creativeSprays/sprays.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/creativeBadges/v_badges.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/creative_props.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "stream/creativeClothes/patoche_shop_list.ytyp"
