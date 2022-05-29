35bc3ed6fe543a18977f6ca948f046ac559a6109
Lucas Hen <oisouhen@gmail.com>
2022-05-29 20:48:38 -0300

    Update fxmanifest.lua

--- server/resources/[addons]/cfx-summerz/fxmanifest.lua
+++ server/resources/[addons]/cfx-summerz/fxmanifest.lua
@@ -17,4 +17,8 @@ data_file "DLC_ITYP_REQUEST" "stream/creativeForestNorth/forest_n_slod.ytyp"
 data_file "DLC_ITYP_REQUEST" "stream/creativeForestSouth/forest_s_slod.ytyp"
 
 data_file "DLC_ITYP_REQUEST" "stream/creativeSprays/sprays.ytyp"
+
 data_file "DLC_ITYP_REQUEST" "stream/creativeBadges/v_badges.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/creative_props.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "stream/creativeClothes/patoche_shop_list.ytyp"
