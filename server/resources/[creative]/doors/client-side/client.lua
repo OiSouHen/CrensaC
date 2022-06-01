3f25fcb5db3c968ee2937714598b7bf0fb082d56
Lucas Hen <oisouhen@gmail.com>
2022-06-01 00:49:41 -0300

    Update doors

--- server/resources/[creative]/doors/client-side/client.lua
+++ server/resources/[creative]/doors/client-side/client.lua
@@ -14,40 +14,42 @@ vSERVER = Tunnel.getInterface("doors")
 CreateThread(function()
 	while true do
 		local timeDistance = 999
-		local ped = PlayerPedId()
-		local coords = GetEntityCoords(ped)
+		if LocalPlayer["state"]["Route"] < 900000 then
+			local ped = PlayerPedId()
+			local coords = GetEntityCoords(ped)
 
-		for k,v in pairs(GlobalState["Doors"]) do
-			local distance = #(coords - vec3(v["x"],v["y"],v["z"]))
-			if distance <= v["distance"] then
-				local closestDoor = GetClosestObjectOfType(v["x"],v["y"],v["z"],v["distance"] + 0.0,v["hash"],false,false,false)
-				if closestDoor then
-					if v["lock"] then
-						local _,h = GetStateOfClosestDoorOfType(v["hash"],v["x"],v["y"],v["z"])
-						if h > -0.02 and h < 0.02 then
-							FreezeEntityPosition(closestDoor,true)
-						end
-					else
-						FreezeEntityPosition(closestDoor,false)
-					end
-
-					if distance <= v["press"] then
-						timeDistance = 0
-
-						if v["text"] then
-							if v["lock"] then
-								DrawText3D(v["x"],v["y"],v["z"],"🔒")
-							else
-								DrawText3D(v["x"],v["y"],v["z"],"🔓")
+			for k,v in pairs(GlobalState["Doors"]) do
+				local distance = #(coords - vec3(v["x"],v["y"],v["z"]))
+				if distance <= v["distance"] then
+					local closestDoor = GetClosestObjectOfType(v["x"],v["y"],v["z"],v["distance"] + 0.0,v["hash"],false,false,false)
+					if closestDoor then
+						if v["lock"] then
+							local _,h = GetStateOfClosestDoorOfType(v["hash"],v["x"],v["y"],v["z"])
+							if h > -0.02 and h < 0.02 then
+								FreezeEntityPosition(closestDoor,true)
 							end
+						else
+							FreezeEntityPosition(closestDoor,false)
 						end
 
-						if IsControlJustPressed(1,38) and vSERVER.doorsPermission(k) then
-							v["lock"] = not v["lock"]
-							vRP.playAnim(true,{"anim@heists@keycard@","exit"},false)
-							vSERVER.doorsStatistics(k,v["lock"])
-							Wait(350)
-							vRP.stopAnim()
+						if distance <= v["press"] then
+							timeDistance = 0
+
+							if v["text"] then
+								if v["lock"] then
+									DrawText3D(v["x"],v["y"],v["z"],"🔒")
+								else
+									DrawText3D(v["x"],v["y"],v["z"],"🔓")
+								end
+							end
+
+							if IsControlJustPressed(1,38) and vSERVER.doorsPermission(k) then
+								v["lock"] = not v["lock"]
+								vRP.playAnim(true,{"anim@heists@keycard@","exit"},false)
+								vSERVER.doorsStatistics(k,v["lock"])
+								Wait(350)
+								vRP.stopAnim()
+							end
 						end
 					end
 				end
