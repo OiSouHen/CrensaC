56ddfdf870dd4d07c76663d774c568323ade1ae6
Lucas Hen <oisouhen@gmail.com>
2022-06-01 00:49:50 -0300

    Update evidence

--- server/resources/[creative]/evidence/client-side/client.lua
+++ server/resources/[creative]/evidence/client-side/client.lua
@@ -32,20 +32,22 @@ end
 CreateThread(function()
 	while true do
 		local timeDistance = 999
-		local ped = PlayerPedId()
-		if not IsPedInAnyVehicle(ped) and GetSelectedPedWeapon(ped) == GetHashKey("WEAPON_FLASHLIGHT") and IsPlayerFreeAiming(PlayerId()) then
-			local coords = GetEntityCoords(ped)
-			local gridZone = getGridzone(coords["x"],coords["y"])
+		if LocalPlayer["state"]["Route"] < 900000 then
+			local ped = PlayerPedId()
+			if not IsPedInAnyVehicle(ped) and GetSelectedPedWeapon(ped) == GetHashKey("WEAPON_FLASHLIGHT") and IsPlayerFreeAiming(PlayerId()) then
+				local coords = GetEntityCoords(ped)
+				local gridZone = getGridzone(coords["x"],coords["y"])
 
-			if GlobalState["Evidences"][gridZone] then
-				for k,v in pairs(GlobalState["Evidences"][gridZone]) do
-					local distance = #(coords - vec3(v[1]["x"],v[1]["y"],v[1]["z"]))
-					if distance <= 5 then
-						timeDistance = 1
-						DrawMarker(28,v[1]["x"],v[1]["y"],v[1]["z"] + 0.05,0.0,0.0,0.0,180.0,0.0,0.0,0.045,0.045,0.045,v[3][1],v[3][2],v[3][3],200,0,0,0,0)
+				if GlobalState["Evidences"][gridZone] then
+					for k,v in pairs(GlobalState["Evidences"][gridZone]) do
+						local distance = #(coords - vec3(v[1]["x"],v[1]["y"],v[1]["z"]))
+						if distance <= 5 then
+							timeDistance = 1
+							DrawMarker(28,v[1]["x"],v[1]["y"],v[1]["z"] + 0.05,0.0,0.0,0.0,180.0,0.0,0.0,0.045,0.045,0.045,v[3][1],v[3][2],v[3][3],200,0,0,0,0)
 
-						if distance <= 1.2 and IsControlJustPressed(1,38) then
-							TriggerServerEvent("evidence:pickupEvidence",k,gridZone)
+							if distance <= 1.2 and IsControlJustPressed(1,38) then
+								TriggerServerEvent("evidence:pickupEvidence",k,gridZone)
+							end
 						end
 					end
 				end
