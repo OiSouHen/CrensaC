fcd0d14bf2d2da21737dde05f3da85c58cc9a2fe
Lucas Hen <oisouhen@gmail.com>
2022-06-02 17:36:54 -0300

    Update fxmanifest.lua

--- server/resources/[addons]/cfx-summerz/fxmanifest.lua
+++ server/resources/[addons]/cfx-summerz/fxmanifest.lua
@@ -13,12 +13,12 @@ files {
 
 data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
 
-data_file "DLC_ITYP_REQUEST" "stream/creativeForestNorth/forest_n_slod.ytyp"
-data_file "DLC_ITYP_REQUEST" "stream/creativeForestSouth/forest_s_slod.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/forest_n_slod.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/forest_s_slod.ytyp"
 
-data_file "DLC_ITYP_REQUEST" "stream/creativeSprays/sprays.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/sprays.ytyp"
 
-data_file "DLC_ITYP_REQUEST" "stream/creativeBadges/v_badges.ytyp"
-data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/creative_props.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/v_badges.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/creative_props.ytyp"
 
-data_file "DLC_ITYP_REQUEST" "stream/creativeClothes/patoche_shop_list.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/patoche_shop_list.ytyp"
