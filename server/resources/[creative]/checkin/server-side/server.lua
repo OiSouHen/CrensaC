0eaa6ad855bd5cbc0d719ce0f21feee462b81be5
Lucas Hen <oisouhen@gmail.com>
2022-06-08 16:11:45 -0300

    Update server.lua

--- server/resources/[creative]/checkin/server-side/server.lua
+++ server/resources/[creative]/checkin/server-side/server.lua
@@ -16,17 +16,13 @@ function cRP.paymentCheckin()
 	local source = source
 	local user_id = vRP.getUserId(source)
 	if user_id then
-		if vRP.getHealth(source) <= 101 then
-			if vRP.tryGetInventoryItem(user_id,"adrenaline",1) then
-				vRP.upgradeHunger(user_id,20)
-				vRP.upgradeThirst(user_id,20)
-				vRP.upgradeStress(user_id,10)
-				TriggerEvent("Repose",source,user_id,900)
+		if vRP.getHealth(source) <= 100 then
+			vRP.upgradeHunger(user_id,20)
+			vRP.upgradeThirst(user_id,20)
+			vRP.upgradeStress(user_id,10)
+			TriggerEvent("Repose",source,user_id,900)
 
-				return true
-			else
-				TriggerClientEvent("Notify",source,"vermelho","Precisa de uma <b>Adrenalina</b>.",5000)
-			end
+			return true
 		else
 			if vRP.request(source,"Prosseguir o tratamento por <b>$750</b> dólares?","Sim","Não") then
 				if vRP.paymentFull(user_id,750) then
