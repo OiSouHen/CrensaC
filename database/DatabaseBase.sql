3226b597231efc6eb14c8ab3ca0e0e7feb340b50
Lucas Hen <oisouhen@gmail.com>
2022-06-22 15:23:49 -0300

    Update DatabaseBase.sql

--- database/DatabaseBase.sql
+++ database/DatabaseBase.sql
@@ -195,7 +195,6 @@ CREATE TABLE IF NOT EXISTS `summerz_propertys` (
   `tax` int(20) NOT NULL DEFAULT '0',
   `user_id` int(11) NOT NULL DEFAULT '0',
   `price` int(20) NOT NULL DEFAULT '0',
-  `residents` int(1) NOT NULL DEFAULT '1',
   `vault` int(10) NOT NULL DEFAULT '1',
   `fridge` int(10) NOT NULL DEFAULT '1',
   `owner` int(1) NOT NULL DEFAULT '0',
